77d28e908e0e15e0e6cde20c53d1500cfcb82e4b
shubham <shubhamagrahari18@gmail.com>
2024-09-13 01:32:57 +0530

    dynamic integration React and backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,29 @@ import './App.css'
 import Home from './pages/home/home'
 import Profile from './pages/Profile/Profile'
 import Login from './pages/LogIn/Login'
+import {Route, Routes, Navigate} from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
 
 
 function App() {
+  const user = useSelector ((state) => state.auth.authData)
+  
+  console.log(user)
   
   return (
-    <>
+    <div className="App">
     <div className="blur" style={{top:'-8%', right:'0'}}></div>
     <div className="blur" style={{top:'36%', left:'-8rem'}}></div>
-    {/* <Home/> */}
-    {/* <Profile/> */}
-    <Login/>
-    </>
+    <Routes>
+      <Route path='/' element={user?<Navigate to= "home"/> : <Navigate to= "login"/>}/>
+      <Route path='/home' element={user? <Home/>:<Navigate to = "../login"/>}/>
+      <Route path='/login' element={user ? <Navigate to= "../home"/> : <Login/>}/>
+      <Route path='/profile/:id' element={user ?  <Profile/> : <Navigate to= "../login"/> }/>
+
+
+    </Routes>
+    </div>
   )
 }
 
--- client/src/components/Feed/Feed.jsx
+++ client/src/components/Feed/Feed.jsx
@@ -1,19 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import './Feed.css';
 import {  UilShareAlt, UilThumbsUp,UilCommentAlt, UilThumbsDown } from '@iconscout/react-unicons';
+import { useSelector } from "react-redux";
+import { likePost } from "../../api/PostRequest";
+// import { likePost } from "../../../../server/Controllers/PostControlller";
 
 const Feed = ({data}) => {
+
+
+    const decodeJwt = (token) => {
+        try {
+          const [, payload] = token.split('.');
+          const decoded = JSON.parse(atob(payload));
+          return decoded;
+        } catch (error) {
+          console.error('Error decoding token:', error);
+          return null;
+        }
+      };
+
+    const user = decodeJwt(useSelector((state) => state.auth.authData))
+
+    const [likeed, setLiked] = useState(data.likes.includes(user.id))
+    const [likes, setLikes] = useState(data.likes.length)
+
+    const handelLike = () =>{
+        setLiked((prev) => !prev )
+        likePost(data._id, user.id)
+        likeed?setLikes((prev) => prev -1):setLikes((prev)=>prev+1)
+    }
     return(
         <div className="Feed">
-            <img src={data.img} alt="" />
+
+            {/* <img src={data.img ? process.env.REACT_APP_PUBLIC_FOLDER + data.image :""} alt="" /> unable to resolve process file value */} 
+            <img src={data.image ? "http://localhost:5002/images/" + data.image :""} alt="" />
+
 
             <div className="feedReact">
-              {data.liked ===true ?  <UilThumbsUp color="green" title="Like" /> :  <UilThumbsDown color="red" title="Dislike" />}
+              {likeed ? <UilThumbsUp color="green" title="Like"  style={{cursor:"pointer"}} onClick={handelLike}/> :  <UilThumbsDown color="red" title="Dislike" style={{cursor:"pointer"}} onClick={handelLike}/>}
              <UilCommentAlt  title="Comment" />
              <UilShareAlt title="Share" />
             </div>
-            <span>{data.likes} Likes</span>
-            <div className="detai">
+            <span>{likes} </span>
+            <div className="detai"> 
                 <span><b>{data.name} </b></span>
                 <span> {data.desc}</span>
             </div>
